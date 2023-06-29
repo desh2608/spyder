2ec247baf4c7ae182500c72c8898171ea70ab25f
Desh Raj <r.desh26@gmail.com>
2023-06-29 21:54:30 +0200

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pybind11 import get_cmake_dir
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import find_packages, setup
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 # The main interface is through Pybind11Extension.
 # * You can add cxx_std=11/14/17, and then build_ext can be removed.
